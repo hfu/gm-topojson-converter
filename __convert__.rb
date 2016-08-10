16d18111b1ecaef5b9a014cf0bfe63c5585c57a4
Hidenori FUJIMURA <hfu@si.local>
2016-08-10 13:04:36 +0900

    update

--- __convert__.rb
+++ __convert__.rb
@@ -1,12 +1,11 @@
 require 'find'
-require './config.rb'
+require './_config_.rb'
 
 targets = []
-Find.find("../../#{$source}/") {|path|
+Find.find("../#{$source}/") {|path|
   next unless path.end_with?('shp')
   target = File.basename(path.sub('shp', 'geojson'))
   targets << target
   print "ogr2ogr -f GeoJSON #{target} #{path}\n"
   print "topojson -o #{target.sub('geojson', 'topojson')} #{target}\n"
 }
-print "topojson -o #{$source}.topojson #{targets.join(' ')}\n"
